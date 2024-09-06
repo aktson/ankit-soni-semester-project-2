b4dde992e33fa8a03bc98229df316dfe195969e6
Ankit Soni <79087448+aktson@users.noreply.github.com>
2024-09-06 17:42:17 +0200

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://ankson.up.railway.app/"
-
+// export const baseUrl = "https://ankson.up.railway.app/"
+export const baseUrl = "https://portfolio-railway.onrender.com/"
